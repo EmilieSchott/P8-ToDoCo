2453ad110753da11244b866b6ca5f55423ff0904
Emilie Schott <emilie.schott@gmail.com>
2021-08-28 17:59:23 +0200

    Add migration file to create roles property in user table in database and give admin role to user with id = 1

---
+++ src/AppBundle/Migrations/Version20210828124134.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210828124134 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Add roles property un the user table and add the admin role to an user with id 1.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD roles JSON NOT NULL');
+        $this->addSql('UPDATE user SET roles = JSON_ARRAY("ROLE_ADMIN") WHERE id=1');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP roles');
+    }
+}
